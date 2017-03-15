8dc2d2bf6a54101fe099bff009ce00cf340cfc5a
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2017-03-15 10:25:03 -0400

    table code moved to other file

--- vector.scm
+++ vector.scm
@@ -1,49 +1,5 @@
 (load "~/.schemerc.scm")
-
-(define (make-table)
-  (list 'table 'end))
-
-(define (table? tbl)
-  (eq? 'table (car tbl)))
-
-(define (table-set! table key value)
-  (assert (table? table) "not a table")
-  (define (set-iter tbl)
-    (cond ((eq? 'end (car tbl)) (insert! tbl (list key value)))
-          ((eq? (caar tbl) key) (set-cdr! (car tbl) (list value)))
-          (else (set-iter (cdr tbl)))))
-  (set-iter (cdr table)))
-
-(define (table-get table key)
-  (assert (table? table) "not a table")
-  (define (get-iter tbl)
-    (cond ((eq? 'end (car tbl)) (error "TABLE-GET -- key not found"))
-          ((eq? (caar tbl) key) (cadar tbl))
-          (else (get-iter (cdr tbl)))))
-  (get-iter (cdr table)))
-
-(define (table-safe-get table key)
-  (assert (table? table) "not a table")
-  (define (get-iter tbl)
-    (cond ((eq? 'end (car tbl)) false)
-          ((eq? (caar tbl) key) (cadar tbl))
-          (else (get-iter (cdr tbl)))))
-  (get-iter (cdr table)))
-
-(define (table-has-key table key)
-  (assert (table? table) "not a table")
-  (define (get-iter tbl)
-    (cond ((eq? 'end (car tbl)) false)
-          ((eq? (caar tbl) key) true)
-          (else (get-iter (cdr tbl)))))
-  (get-iter (cdr table)))
-
-(define (table-memoize! table key thunk)
-  (if (table-has-key table key)
-    (table-get table key)
-    (let ((val (thunk)))
-      (table-set! table key val)
-      val)))
+(load-module "sicp.table")
 
 (define (make-vector coords)
   (list 'vector coords (make-table)))
@@ -63,7 +19,7 @@
   (nth n (vector-coords vec)))
 
 (define (vector-dimension vec)
-  (table-memoize! (vector-cache vec)
+  (table-getset! (vector-cache vec)
                   'dimension
                   (lambda () (length (vector-coords vec)))))
 
@@ -94,13 +50,13 @@
     (fold + 0 (map (lambda (t) (* (car t) (cadr t))) (zip c1 c2)))))
 
 (define (vector-magnitude vec)
-  (table-memoize!
+  (table-getset!
     (vector-cache vec)
     'magnitude
     (lambda () (sqrt (fold + 0 (map (lambda (x) (* x x)) (vector-coords vec)))))))
 
 (define (vector-normalize vec)
-  (table-memoize!
+  (table-getset!
     (vector-cache vec)
     'normalized
     (lambda () (vector-scale vec (/ 1.0 (vector-magnitude vec))))))
